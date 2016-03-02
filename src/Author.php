02fd19e841319d86f77948f6af8ae7f825c09089
Jon Nardine and matt knutson <mknut001@gmail.com>
2016-03-02 10:45:00 -0800

    specs for delete all get all and save pass

--- src/Author.php
+++ src/Author.php
@@ -1,29 +1,105 @@
 <?php
     class Author
     {
-        private $author;
+        private $author_name;
         private $id;
 
-        function __construct($author, $id = null)
+        function __construct($author_name, $id = null)
         {
-            $this->author = $author;
+            $this->author_name = $author_name;
             $this->id = $id;
         }
 
-        function setAuthor($new_author)
+        function setAuthor_Name($new_author_name)
         {
-            $this->author = (string) $new_author;
+            $this->author_name = (string) $new_author_name;
         }
 
-        function getAuthor()
+        function getAuthor_Name()
         {
-            return $this->author;
+            return $this->author_name;
         }
 
         function getId()
         {
             return $this->id;
         }
+
+        function save()
+        {
+              $GLOBALS['DB']->exec("INSERT INTO authors (author)  VALUES ('{$this->getAuthor_Name()}');");
+              $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_authors = $GLOBALS['DB']->query("SELECT * FROM authors;");
+            $authors = array();
+            foreach($returned_authors as $author) {
+                $author_name = $author['author'];
+                $id = $author['id'];
+                $new_author = new Author($author_name, $id);
+                array_push($authors, $new_author);
+            }
+            return $authors;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM authors;");
+        }
+
+        static function find($search_id)
+        {
+            $found_author = null;
+            $authors = Author::getAll();
+            foreach($authors as $author) {
+                $author_id = $author->getId();
+                if ($author_id == $search_id) {
+                  $found_author = $author;
+                }
+            }
+            return $found_author;
+        }
+
+        function update($new_author_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE authors SET author = '{$new_author_name}' WHERE id = {$this->getId()};");
+            $this->setAuthor($new_author_name);
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM authors WHERE id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM authors_books WHERE author_id = {$this->getId()};");
+        }
+        //
+        // function addBook($book)
+        // {
+        //     $GLOBALS['DB']->exec("INSERT INTO authors_books (author_id, book_id) VALUES ({$this->getId()}, {$book->getId()});");
+        // }
+        //
+        // function books()
+        // {
+        //     $query = $GLOBALS['DB']->query("SELECT book_id FROM authors_books WHERE author_id = {$this->getId()};");
+        //     $book_ids = $query->fetchAll(PDO::FETCH_ASSOC);
+        //     $books = array();
+        //     foreach($book_ids as $id) {
+        //         $book_id = $id['book_id'];
+        //         $result = $GLOBALS['DB']->query("SELECT * FROM books WHERE id = {$book_id};");
+        //         $returned_book = $result->fetchAll(PDO::FETCH_ASSOC);
+        //         $book_name = $returned_book[0]['book_name'];
+        //         $id = $returned_book[0]['id'];
+        //         $new_book = new Book($book_name, $id);
+        //         array_push($books, $new_book);
+        //   }
+        //   return $books;
+        // }
+
+
+
+
+
 
     }
 ?>
--- tests/AuthorTest.php
+++ tests/AuthorTest.php
@@ -14,27 +14,28 @@
     {
         protected function tearDown()
         {
-            // Author::deleteAll()
+            Author::deleteAll();
+
         }
 
-        function testGetAuthor()
+        function testGetAuthor_Name()
         {
-            $author = "Matt";
-            $test_author = new Author($author);
+            $author_name = "Matt";
+            $test_author = new Author($author_name);
 
-            $result = $test_author->GetAuthor();
+            $result = $test_author->getAuthor_Name();
 
-            $this->assertEquals($author, $result);
+            $this->assertEquals($author_name, $result);
         }
 
         function testSetAuthor()
         {
            //Arrange
-           $author = "Matt";
-           $test_author = new Author($author);
+           $author_name = "Matt";
+           $test_author = new Author($author_name);
            //Act
-           $test_author->setAuthor("Matt");
-           $result = $test_author->getAuthor();
+           $test_author->setAuthor_Name("Matt");
+           $result = $test_author->getAuthor_Name();
            //Assert
            $this->assertEquals("Matt", $result);
         }
@@ -42,14 +43,170 @@
        function testGetId()
         {
             //Arrange
-            $author = "Matt";
+            $author_name = "Matt";
             $id = 1;
 
-            $test_author = new Author($author, $id);
+            $test_author = new Author($author_name, $id);
             //Act
             $result = $test_author->getId();
             //Assert
             $this->assertEquals(1, $result);
         }
+
+        function testSave()
+       {
+           //Arrange
+
+           $author_name = "Matt";
+           $test_author = new Author($author_name);
+           //Act
+           $test_author->save();
+           //Assert
+           $result = Author::getAll();
+
+           $this->assertEquals($test_author, $result[0]);
+       }
+
+        function test_getAll()
+         {
+             //Arrange
+             $author_name = "Matt";
+             $author_name2 = "Jon";
+             $test_author = new Author($author_name);
+             $test_author->save();
+             $test_author2 = new Author($author_name2);
+             $test_author2->save();
+             //Act
+             $result = Author::getAll();
+             //Assert
+             $this->assertEquals([$test_author, $test_author2], $result);
+         }
+
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $author_name = "Matt";
+            $id = 1;
+            $test_author = new Author($author_name, $id);
+            $test_author->save();
+            
+            $author_name2 = "Jon";
+            $id2 = 2;
+            $test_author2 = new Author($author_name2, $id2);
+            $test_author2->save();
+            //Act
+            Author::deleteAll();
+            //Assert
+            $result = Author::getAll();
+            $this->assertEquals([], $result);
+        }
+        //
+        // function test_find()
+        // {
+        //     //Arrange
+        //     $author_name = "Matt";
+        //     $author_name2 = "Jon";
+        //     $test_author = new Author($author_name);
+        //     $test_author->save();
+        //     $test_author2 = new Author($author_name2);
+        //     $test_author2->save();
+        //     //Act
+        //     $result = Author::find($test_author->getId());
+        //     //Assert
+        //     $this->assertEquals($test_author, $result);
+        // }
+        //
+        // function testUpdate()
+        //  {
+        //      //Arrange
+        //      $author_name = "Matt";
+        //      $id = 1;
+        //      $test_author = new Author($author_name, $id);
+        //      $test_author->save();
+        //      $new_author = "Matthew";
+        //      //Act
+        //      $test_author->update($new_author_name);
+        //      //Assert
+        //      $this->assertEquals("Matthew", $test_author->getAuthor());
+        //  }
+
+        //  function testAddBook()
+        // {
+        //       //Arrange
+        //       $author_name = "Matt";
+        //       $id = 1;
+        //       $test_author = new Author($author_name, $id);
+        //       $test_author->save();
+        //       $book = "Matt and his guide to stuff";
+        //       $id2 = 2;
+        //       $test_book = new Book($book, $id2;
+        //       $test_book->save();
+        //       //Act
+        //       $test_author->addBook($test_book);
+        //       //Assert
+        //       $this->assertEquals($test_author->books(), [$test_book]);
+        // }
+        //
+        // function testGetBooks()
+        // {
+        // //Arrange
+        //     $author_name = "Matt";
+        //     $id = 1;
+        //     $test_author = new Author($author_name, $id);
+        //     $test_author->save();
+        //     $book = "Matt and his Guide to Stuff";
+        //     $id2 = 2;
+        //     $test_book = new Book($book, $id2);
+        //     $test_book->save();
+        //     $book2 = "Matt and his World";
+        //     $id3 = 3;
+        //     $test_book2 = new Book($book2, $id3);
+        //     $test_book2->save();
+        //     //Act
+        //     $test_author->addBook($test_book);
+        //     $test_author->addBook($test_book2);
+        //     //Assert
+        //     $this->assertEquals($test_author->books(), [$test_book, $test_book2]);
+        // }
+        //
+        //  function testDelete()
+        //  {
+        //      //Arrange
+        //      $author_name = "Matt";
+        //      $id = 1;
+        //      $test_author = new Author($author_name, $id);
+        //      $test_author->save();
+        //      $author_name2 = "Jon";
+        //      $id2 = 2;
+        //      $test_author2 = new Author($author_name2, $id2);
+        //      $test_author2->save();
+        //      //Act
+        //      $test_author->delete();
+        //      //Assert
+        //      $this->assertEquals([$test_author2], Author::getAll());
+        //  }
+        //
+        //  function testBooks()
+        // {
+        //     //Arrange
+        //     $author_name = "Matt";
+        //     $id = null;
+        //     $test_author = new Author($author_name, $id);
+        //     $test_author->save();
+        //     $book = "Matt and his Guide to Stuff";
+        //     $test_book = new Book($book, $id);
+        //     $test_book->save();
+        //     $book2 = "Take out the trash";
+        //     $test_book2 = new Book($book2, $id);
+        //     $test_book2->save();
+        //     //Act
+        //     $test_author->addBook($test_book);
+        //     $test_author->addBook($test_book2);
+        //     //Assert
+        //     $this->assertEquals($test_author->books(), [$test_book, $test_book2]);
+        // }
+
+
     }
 ?>
